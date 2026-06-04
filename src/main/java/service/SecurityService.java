15f0b77b29af679185abc8f24b2deefa65bbbf14
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-06-04 11:07:30 +0300

    fix: remove auto register and add test users

--- src/main/java/service/SecurityService.java
+++ src/main/java/service/SecurityService.java
@@ -8,11 +8,16 @@ import java.util.Map;
 public class SecurityService {
     private final Map<String, User> users = new HashMap<>();
 
+    public SecurityService() {
+        users.put("u1", new User("u1", "u1"));
+        users.put("u2", new User("u2", "u2"));
+        users.put("u3", new User("u3", "u3"));
+    }
+
     public boolean login(String login, String password) {
         User user = users.get(login);
         if (user == null) {
-            register(login, password);
-            return true;
+            return false;
         }
         return user.getPassword().equals(password);
     }
